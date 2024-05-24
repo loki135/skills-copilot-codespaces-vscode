c01dfafafbf2737c61a3f071b4c7c2122c31154e
lokesh <93985411+loki135@users.noreply.github.com>
2024-05-24 10:19:48 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,36 @@
+// create web server
+// http server
+// get request
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+// create server
+http.createServer(function(request, response) {
+  // parse the request containing file name
+  var pathname = url.parse(request.url).pathname;
+  // print the name of the file for which request is made.
+  console.log('Request for ' + pathname + ' received.');
+
+  // read the requested file content from file system
+  fs.readFile(pathname.substr(1), function(err, data) {
+    if (err) {
+      console.log(err);
+      // HTTP Status: 404 : NOT FOUND
+      // Content Type: text/plain
+      response.writeHead(404, {'Content-Type': 'text/html'});
+    } else {
+      // Page found
+      // HTTP Status: 200 : OK
+      // Content Type: text/plain
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      // write the content of the file to response body
+      response.write(data.toString());
+    }
+    // send the response body
+    response.end();
+  });
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://
