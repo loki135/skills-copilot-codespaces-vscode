f059d9d34cc1c6c90ecd3bffe11850e8c11875c3
lokesh <93985411+loki135@users.noreply.github.com>
2024-05-24 10:15:43 +0000

    chore: Add Express server with basic routes for comments

--- comments.js
+++ comments.js
@@ -1,12 +1,78 @@
-const http = require('http');
+// create web server using express
+// npm install express
 
-// create web server
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
+// import express
+const express = require('express');
+// create express app
+const app = express();
+// set the port
+const port = 3000;
+
+// create a get route
+app.get('/', (req, res) => {
+    res.send('Hello World');
 });
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
+// listen to the port
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
+
+// run the server
+// node comments.js
+// open browser and go to http://localhost:3000
+// you should see Hello World
+
+// create a route to get all comments
+app.get('/comments', (req, res) => {
+    res.send('Get all comments');
+});
+
+// create a route to get a comment by id
+app.get('/comments/:id', (req, res) => {
+    res.send('Get comment by id');
+});
+
+// create a route to create a comment
+app.post('/comments', (req, res) => {
+    res.send('Create a comment');
+});
+
+// create a route to update a comment
+app.put('/comments/:id', (req, res) => {
+    res.send('Update a comment');
+});
+
+// create a route to delete a comment
+app.delete('/comments/:id', (req, res) => {
+    res.send('Delete a comment');
+});
+
+// create a route to get all comments by user
+app.get('/users/:userId/comments', (req, res) => {
+    res.send('Get all comments by user');
+});
+
+// create a route to get all comments by user
+app.get('/users/:userId/comments/:commentId', (req, res) => {
+    res.send('Get a comment by user');
+});
+
+// create a route to get all comments by user
+app.post('/users/:userId/comments', (req, res) => {
+    res.send('Create a comment by user');
+});
+
+// create a route to get all comments by user
+app.put('/users/:userId/comments/:commentId', (req, res) => {
+    res.send('Update a comment by user');
+});
+
+// create a route to get all comments by user
+app.delete('/users/:userId/comments/:commentId', (req, res) => {
+    res.send('Delete a comment by user');
+});
+
+// run the server
+// node comments.js
+// open browser and go to http://localhost:3000/comments
