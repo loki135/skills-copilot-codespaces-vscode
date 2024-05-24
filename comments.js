b53724201c735a024569ecfe5daced8e240a6c27
lokesh <93985411+loki135@users.noreply.github.com>
2024-05-24 10:24:10 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,36 +1,75 @@
-// create web server
-// http server
-// get request
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-// create server
-http.createServer(function(request, response) {
-  // parse the request containing file name
-  var pathname = url.parse(request.url).pathname;
-  // print the name of the file for which request is made.
-  console.log('Request for ' + pathname + ' received.');
-
-  // read the requested file content from file system
-  fs.readFile(pathname.substr(1), function(err, data) {
-    if (err) {
-      console.log(err);
-      // HTTP Status: 404 : NOT FOUND
-      // Content Type: text/plain
-      response.writeHead(404, {'Content-Type': 'text/html'});
-    } else {
-      // Page found
-      // HTTP Status: 200 : OK
-      // Content Type: text/plain
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      // write the content of the file to response body
-      response.write(data.toString());
-    }
-    // send the response body
-    response.end();
-  });
-}).listen(8081);
-
-// Console will print the message
-console.log('Server running at http://
+// create web server with express
+var express = require('express');
+var app = express();
+// create a route for the app
+app.get('/comments', function(req, res) {
+  res.send('This is a page for comments');
+});
+// make the server listen to requests
+app.listen(3000);
+// Path: index.js
+// create web server with express
+var express = require('express');
+var app = express();
+// create a route for the app
+app.get('/', function(req, res) {
+  res.send('Welcome to my website!');
+});
+// make the server listen to requests
+app.listen(3000);
+// Path: index.js
+// create web server with express
+var express = require('express');
+var app = express();
+// create a route for the app
+app.get('/', function(req, res) {
+  res.send('Welcome to my website!');
+});
+// make the server listen to requests
+app.listen(3000);
+// Path: index.js
+// create web server with express
+var express = require('express');
+var app = express();
+// create a route for the app
+app.get('/', function(req, res) {
+  res.send('Welcome to my website!');
+});
+// make the server listen to requests
+app.listen(3000);
+// Path: index.js
+// create web server with express
+var express = require('express');
+var app = express();
+// create a route for the app
+app.get('/', function(req, res) {
+  res.send('Welcome to my website!');
+});
+// make the server listen to requests
+app.listen(3000);
+// Path: index.js
+// create web server with express
+var express = require('express');
+var app = express();
+// create a route for the app
+app.get('/', function(req, res) {
+  res.send('Welcome to my website!');
+});
+// make the server listen to requests
+app.listen(3000);
+// Path: index.js
+// create web server with express
+var express = require('express');
+var app = express();
+// create a route for the app
+app.get('/', function(req, res) {
+  res.send('Welcome to my website!');
+});
+// make the server listen to requests
+app.listen(3000);
+// Path: index.js
+// create web server with express
+var express = require('express');
+var app = express();
+// create a route for the app
+app.get('/', function(req, res) {
